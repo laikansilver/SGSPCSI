0d03d85af7c55d7f0ef85e7b9bcd1c0d21d550e7
jacquihuhe <l21121538@morelia.tecnm.mx>
2026-04-27 11:13:41 -0600

    Frontend-login

---
+++ Frontend/src/app/pages/developer-home/developer-home.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-developer-home',
+  imports: [CommonModule],
+  templateUrl: './developer-home.component.html',
+  styleUrl: './developer-home.component.scss'
+})
+export class DeveloperHomeComponent {}
